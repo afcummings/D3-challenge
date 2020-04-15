cb1f6739527698bb155e6edab4a99e3f3ea3908f
afcummings <56271984+afcummings@users.noreply.github.com>
2020-04-15 18:30:46 -0400

    working d3 chart

---
+++ assets/js/app.js
@@ -0,0 +1,110 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+
+// Import Data
+d3.csv("data.csv", function(err, healthData) {
+  if (err) throw err;
+  healthData.forEach(function(data) {
+    data.obesity = +data.obesity;
+    data.healthcare = +data.healthcare;
+  });
+
+  var xLinearScale = d3.scaleLinear().range([0, width]);
+  var yLinearScale = d3.scaleLinear().range([height, 0]);
+
+
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  var xMin;
+  var xMax;
+  var yMin;
+  var yMax;
+  
+  xMin = d3.min(healthData, function(data) {
+      return data.healthcare;
+  });
+  
+  xMax = d3.max(healthData, function(data) {
+      return data.healthcare;
+  });
+  
+  yMin = d3.min(healthData, function(data) {
+      return data.obesity;
+  });
+  
+  yMax = d3.max(healthData, function(data) {
+      return data.obesity;
+  });
+  
+  xLinearScale.domain([xMin, xMax]);
+  yLinearScale.domain([yMin, yMax]);
+  console.log(xMin);
+  console.log(yMax);
+
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  var circlesGroup = chartGroup.selectAll("circle")
+  .data(healthData)
+  .enter()
+  .append("circle")
+  .attr("cx", d => xLinearScale(d.healthcare +1.5))
+  .attr("cy", d => yLinearScale(d.obesity +0.3))
+  .attr("r", "12")
+  .attr("fill", "blue")
+  .attr("opacity", .5)
+
+  chartGroup.append("text")
+  .style("font-size", "12px")
+  .selectAll("tspan")
+  .data(healthData)
+  .enter()
+  .append("tspan")
+      .attr("x", function(data) {
+          return xLinearScale(data.healthcare +1.3);
+      })
+      .attr("y", function(data) {
+          return yLinearScale(data.obesity +.1);
+      })
+      .text(function(data) {
+          return data.abbr
+      });
+
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Lacks Healtcare(%)");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("Obese (%)");
+});
